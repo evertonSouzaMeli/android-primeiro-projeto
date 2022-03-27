21f354adb4b07d8aa68f32183a67ad9e4ddd90d1
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-26 21:26:07 -0300

    feat: work aula-02

--- app/src/main/java/br/com/fiap/primeiroprojeto/MainActivity.kt
+++ app/src/main/java/br/com/fiap/primeiroprojeto/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cliqueBtn(view: View){
-        var msg = "Bem vindo, ${editNome.text}"
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        if(editNome.text.trim().isEmpty()){
+            var errorMsg = "Informe um nome"
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show()
+        }else {
+            var msg = "Bem vindo, ${editNome.text}"
+            exibirMensagem("Saudação", msg)
+        }
+    }
+
+    fun exibirMensagem(titulo: String, mensagem: String){
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(titulo)
+               .setMessage(mensagem)
+               .setPositiveButton("fechar".uppercase(Locale.getDefault()), null)
+
+        builder.create().show()
     }
 }
